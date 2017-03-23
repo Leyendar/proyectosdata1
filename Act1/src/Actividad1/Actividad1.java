93829046fbed85cb6efd834bf5d02d5fb2e03aa8
S.Roberto Padilla <s.robertopadilla@gmail.com>
2017-03-23 19:55:58 +0100

    corrige el codigo

--- Act1/src/Actividad1/Actividad1.java
+++ Act1/src/Actividad1/Actividad1.java
@@ -17,8 +17,8 @@ public class Actividad1 {
 	}
 		System.out.println(numerosParesImpares);
 	}
-	
-	
+	//OLA K ASE
+	//CODDIGO MAL HECHO
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random rnd=new Random();
